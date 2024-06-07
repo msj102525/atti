306962fd6e01d2575ff3980d45530a1ed95477b4
yuri0330 <kyl1563@naver.com>
2024-06-07 16:44:05 +0900

    user controller

--- atti_boot/src/main/java/org/ict/atti_boot/user/controller/UserContorller.java
+++ atti_boot/src/main/java/org/ict/atti_boot/user/controller/UserContorller.java
@@ -3,14 +3,28 @@ package org.ict.atti_boot.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.ict.atti_boot.user.jpa.entity.User;
+import org.ict.atti_boot.user.model.input.InputUser;
+import org.ict.atti_boot.user.model.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/users")
-@RequiredArgsConstructor
 @CrossOrigin
 public class UserContorller {
+
+    private final UserService userService;
+
+    public UserContorller(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<?> signUpUser(@RequestBody InputUser user) {
+        User newUser = userService.signUpUser(user);
+        return ResponseEntity.ok(newUser);
+    }
+
 }
