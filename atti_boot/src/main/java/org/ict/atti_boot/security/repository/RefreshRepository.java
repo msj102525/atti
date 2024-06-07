5c64c0facd10a9523ba78f77fca847e365eb93c1
yuri0330 <kyl1563@naver.com>
2024-06-07 11:56:25 +0900

    boot project

---
+++ atti_boot/src/main/java/org/ict/atti_boot/security/repository/RefreshRepository.java
@@ -0,0 +1,20 @@
+package org.ict.atti_boot.security.repository;
+
+import org.ict.atti_boot.security.model.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface RefreshRepository extends JpaRepository<RefreshToken, UUID> {
+    Optional<RefreshToken> findByTokenValue(String tokenValue);
+
+    Boolean existsByTokenValue(String refresh);
+
+    void deleteByTokenValue(String refresh);
+
+    Optional<RefreshToken> findByUserId(UUID id);
+
+}
---
+++ atti_boot/src/main/java/org/ict/atti_boot/user/model/service/UserService.java
@@ -0,0 +1,28 @@
+package org.ict.atti_boot.user.model.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.ict.atti_boot.user.jpa.entity.User;
+import org.ict.atti_boot.user.jpa.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+@Transactional
+public class UserService {
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserRepository userRepository;
+
+    public UserService(BCryptPasswordEncoder bCryptPasswordEncoder, UserRepository userRepository) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.userRepository = userRepository;
+    }
+
+
+    public Optional<User> findByEmail(String username) {
+        return userRepository.findByEmail(username);
+    }
+}
